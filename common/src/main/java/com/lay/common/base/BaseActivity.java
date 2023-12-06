3a2ad960682dc45ce1edb76af3d77a6fd4170519
周金磊 <978100606@qq.com>
2023-12-06 16:25:45 +0800

    feat: 相关依赖变更。

--- common/src/main/java/com/lay/common/base/BaseActivity.java
+++ common/src/main/java/com/lay/common/base/BaseActivity.java
@@ -4,9 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Looper;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.lay.common.util.ActivityUtils;
 
